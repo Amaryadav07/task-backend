06beb4c8f77ec7842e6199b97a0765fb20bfeb7f
Amaryadav07 <anath2893007@gmail.com>
2025-06-24 19:12:42 +0530

    added

--- Config/dbconn.js
+++ Config/dbconn.js
@@ -6,6 +6,13 @@ const connectDB = async () => {
     await mongoose.connect(process.env.DBCON, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+        ssl: true,
+  tlsInsecure: false,
+  tlsAllowInvalidCertificates: false,
+  tlsAllowInvalidHostnames: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: '1'
     });
     console.log('MongoDB connected successfully!');
   } catch (err) {
